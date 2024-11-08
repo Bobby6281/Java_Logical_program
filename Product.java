e19190375ec60799cf25ffffd6ff777f3f25705b
Bobby6281 <peddibabi24@gmail.com>
2024-11-08 07:49:19 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Product
+{
+public static void main(String args[])
+{
+int Pno=31;
+String Pname="Bobby";
+String Plocation="Vijayawada";
+float Pprice=23.44F;
+int Pquantity=5;
+System.out.println("product  number is:"+Pno);
+System.out.println("product  name is:"+Pname);
+System.out.println("product location is:"+Plocation);
+System.out.println("product price is:"+Pprice);
+System.out.println("product quantity is:"+Pquantity);
+}
+}
---
+++ Student.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Student
+{
+public static void main(String args[])
+{
+int sno=31;
+String sname="Bobby";
+String slocation="Vijayawada";
+String scollege="SRK";
+String suniversity="Jntuk";
+System.out.println("student roll number is:"+sno);
+System.out.println("student  name is:"+sname);
+System.out.println("student location is:"+slocation);
+System.out.println("student university is:"+suniversity);
+System.out.println("student college is:"+scollege);
+}
+}
